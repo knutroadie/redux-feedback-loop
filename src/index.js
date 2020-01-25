38f001526195180e5b55f71243cd650bfeaf8dc9
luke rohde <morneau33@gmail.com>
2020-01-25 12:49:36 -0600

    added dependecies, to-do list

--- src/index.js
+++ src/index.js
@@ -3,6 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import Feeling from './components/Feeling/Feeling';
+import Supported from './components/Supported/Supported';
+
+Feeling
+Understanding
+Supported
+Comments
+
+Combiner
+
+Provider
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
