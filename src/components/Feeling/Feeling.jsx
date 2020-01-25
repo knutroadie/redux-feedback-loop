366006358cf181019d6fb766eb8420f30fc4ad6f
luke rohde <morneau33@gmail.com>
2020-01-25 15:27:39 -0600

    submitting a feeling

--- src/components/Feeling/Feeling.jsx
+++ src/components/Feeling/Feeling.jsx
@@ -1,18 +1,52 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
 import '../App/App.css';
+import { HashRouter as Router, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+
 
 class Feeling extends Component {
+
+    //state
+    state = {
+        feelingNumber: 0
+    }
+    //input function
+    makeFeeling = (event, feelingNumber) => {
+        console.log('making a feeling!', feelingNumber);
+        
+        this.setState({
+            feelingNumber: event.target.value
+        })
+    }
+
+    //click listener
+    dispatchFeeling = () => {
+        console.log(this.state);
+        this.props.dispatch({
+            type: 'FEELING',
+            payload: this.state.feelingNumber
+        });
+        // this.props.history.push('/');
+    }
+
+
     render() {
         return (
             <>
                 <div className="App"></div>
                 <h3>how do you feel today?</h3>
-                <input type="number" name="quantity" min="1" max="5"></input>
-                <button>next</button>
+                <input type="number" name="quantity" min="1" max="5" value={this.state.feeling} onChange={(event) => this.makeFeeling(event, 'feelingNumber')}></input>
+                <button onClick={this.dispatchFeeling}>next</button>
             </>
         );
     }
 }
 
-export default Feeling;
+const putReduxStateOnProps = (reduxState) => {
+    return {
+        reduxState
+    }
+}
+
+export default connect(putReduxStateOnProps)(Feeling);
