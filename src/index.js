a557f83cab9d194652c43bb97435f7bca9667eca
luke rohde <morneau33@gmail.com>
2020-01-25 13:03:52 -0600

    added 5 reducers

--- src/index.js
+++ src/index.js
@@ -2,18 +2,62 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
+import logger from 'redux-logger';
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux';
 import registerServiceWorker from './registerServiceWorker';
-import Feeling from './components/Feeling/Feeling';
-import Supported from './components/Supported/Supported';
 
-Feeling
-Understanding
-Supported
-Comments
+// Feeling
+const Feeling = (state = 0, action) => {
+    if (action.type === 'FEELING') {
+        return action.payload
+    }
+    return state;
+}
 
-Combiner
+// Understanding
+const Understanding = (state = 0, action) => {
+    if (action.type === 'UNDERSTANDING') {
+        return action.payload
+    }
+    return state;
+}
 
-Provider
+// Supported
+const Supported = (state = 0, action) => {
+    if (action.type === 'SUPPORTED') {
+        return action.payload
+    }
+    return state;
+}
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Comments
+const Comments = (state = '', action) => {
+    if (action.type === 'COMMENTS') {
+        return action.payload
+    }
+    return state;
+}
+
+// Submit
+const Submit = (state = [], action) => {
+    if (action.type === 'SUBMIT') {
+        return action.payload
+    }
+    return state;
+}
+
+const storeInstance = createStore(
+    combineReducers({
+        Feeling,
+        Understand,
+        Supported,
+        Comments,
+        Submit
+    }),
+    applyMiddleware(logger),
+)
+
+ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
+
 registerServiceWorker();
