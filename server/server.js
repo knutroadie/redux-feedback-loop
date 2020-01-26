ba0bc5831327c2e5973102d9b7182983f46268b0
luke rohde <morneau33@gmail.com>
2020-01-26 15:45:56 -0600

    it appears as though all is working now

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000;
 const router = express.Router();
+const pool = require( './modules/pool' );
 
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); // needed for angular requests
@@ -10,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('build'));
 
 /** ---------- EXPRESS ROUTES ---------- **/
-// app.use('/api/feedback', require('/api/feedback'));
+// app.use('/api/feedback', require('/feedback'));
 app.post('/feedback', (req, res) => {
     console.log('in /feedback POST:', req.body);
     const query = `INSERT INTO "feedback" ( "feeling", "understanding", "support", "comments" ) VALUES ( $1, $2, $3, $4 );`;
--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -6,66 +6,11 @@ import { HashRouter as Router, Link } from 'react-router-dom';
 
 class Review extends Component {
 
-    //state
-    // state = {
-    //     feedback: {
-    //         feeling: 0,
-    //         understanding: 0,
-    //         supported: 0,
-    //         comments: ''
-    //     },
-    // };
-    // // setState function
-    // makeFeedback = () => {
-    //     console.log(this.state.feedback, this.props.reduxState);
-    //     this.setState({
-    //         feedback: {
-    //             ...this.state.feedback,
-    //             feeling: this.props.reduxState.Feeling,
-    //             understanding: this.props.reduxState.Understanding,
-    //             supported: this.props.reduxState.Supported,
-    //             comments: this.props.reduxState.Comments
-    //         }
-    //     })
-    //     console.log(this.state.feedback);        
-    //     this.dispatchFeedback()
-    // }
-    // click listener
-    // dispatchFeedback = () => {
-    //     console.log('hit dispatch', this.state.feedback);
-    //     this.props.dispatch({
-    //         type: 'SUBMIT',
-    //         payload: this.state.feedback
-    //     });
-    //     // this.props.history.push('/');
-    // }
-
-
-    //axios post request
-    //     axios.post('/feedback', (req, res) => {
-    //         console.log(req.body);
-    //         let feedback = req.body;
-    //         //sanitize the db insert
-    //         let queryText = `
-    //           INSERT INTO "Koala Table"("gender", "name", "age", "readyToTransfer", "notes")
-    //           VALUES ($1, $2, $3, $4, $5);
-    //           `;
-    //         pool.query(queryText, [addKoala.gender, addKoala.name, addKoala.age, addKoala.readyToTransfer, addKoala.notes])
-    //             .then((result) => {
-    //                 res.sendStatus(201);
-    //             })
-    //             .catch((error) => {
-    //                 console.log(error);
-    //                 res.sendStatus(500);
-    //             });
-
-    //     });
-
     postFeedback = () => {
         let feedback = {
             feeling: this.props.reduxState.Feeling,
             understanding: this.props.reduxState.Understanding,
-            supported: this.props.reduxState.Supported,
+            support: this.props.reduxState.Supported,
             comments: this.props.reduxState.Comments
         }
         // event.preventDefault();
